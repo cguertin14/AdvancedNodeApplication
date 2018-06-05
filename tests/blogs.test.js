71d1e0171d8eab523049b73050f9ce02ade358dc
Charles <charlesguertin@live.ca>
2018-06-05 17:22:19 -0400

    Tests done, performance enhanced with global requests testers method inside of Page class.

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -12,7 +12,6 @@ afterEach(async () => {
 });
 
 describe('When logged in', async () => {
-
     beforeEach(async () => {
         await page.login();
         await page.click('a.btn-floating');
@@ -24,4 +23,68 @@ describe('When logged in', async () => {
         expect(label).toEqual('Blog Title');
     });
 
+    describe('And using valid inputs', async () => {
+        beforeEach(async () => {
+            await page.type('.title input', 'My Title');
+            await page.type('.content input', 'My Content');
+            await page.click('form button');
+        });
+
+        test('Submitting takes user to review screen', async () => {
+            const text = await page.getContentsOf('h5');
+
+            expect(text).toEqual('Please confirm your entries');
+        });
+
+        test('Submitting then saving adds blog to index page', async () => {
+            await page.click('button.green');
+            await page.waitFor('.card');
+
+            const cardTitle = await page.getContentsOf('.card-title');
+            const cardContent = await page.getContentsOf('p');
+
+            expect(cardTitle).toEqual('My Title');
+            expect(cardContent).toEqual('My Content');
+        });
+    });
+
+    describe('And using invalid Inputs', async () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+
+        test('the form shows an error message', async () => {
+            const titleError = await page.getContentsOf('.title .red-text');
+            const contentError = await page.getContentsOf('.content .red-text');
+
+            expect(titleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        });
+    });
+
+});
+
+describe('When user is not logged in', async () => {
+    const actions = [
+        {
+            method: 'get',
+            path: '/api/blogs'
+        },
+        {
+            method: 'post',
+            path: '/api/blogs',
+            data: {
+                title: 'My Title',
+                content: 'My Content'
+            }
+        }
+    ];
+
+    test('Blog related actions are prohibited', async () => {
+        const results = await page.execRequests(actions);
+
+        for (let result of results) {
+            expect(result).toEqual({ error: 'You must log in!' });
+        }
+    });
 });
